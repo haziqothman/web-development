45231b344467ab7c753d6b27d12ccaaf4f9981dc
haziq <haziq.othman99@gmail.com>
2020-06-24 23:54:13 +0800

    Responsive function

--- app.js
+++ app.js
@@ -1,17 +1,78 @@
-function onload() {
-  //   alert("Function onload() called");
-  document.getElementById("timestamp").innerHTML = Date();
+// function onload() {
+//   //   alert("Function onload() called");
+//   document.getElementById("timestamp").innerHTML = Date();
+// }
+// function checkNumber() {
+//   var theNumber, theMessage;
+
+//   theNumber = document.getElementById("smallnumber").value;
+
+//   if (isNaN(theNumber) || theNumber < 1 || theNumber > 10) {
+//     theMessage = "Number was expected to be betwwen 1 to 10";
+//   } else {
+//     theMessage = "Number is good";
+//   }
+
+//   document.getElementById("numberMessage").innerHTML = theMessage;
+// }
+const sizes = document.querySelectorAll(".size");
+const colors = document.querySelectorAll(".color");
+const shoes = document.querySelectorAll(".shoe");
+const gradients = document.querySelectorAll(".gradient");
+const shoeBg = document.querySelector(".shoeBackground");
+
+let prevColor = "blue";
+let animationEnd = true;
+
+function changeSize() {
+  sizes.forEach((size) => size.classList.remove("active"));
+  this.classList.add("active");
 }
-function checkNumber() {
-  var theNumber, theMessage;
 
-  theNumber = document.getElementById("smallnumber").value;
+function changeColor() {
+  if (!animationEnd) return;
+  let primary = this.getAttribute("primary");
+  let color = this.getAttribute("color");
+  let shoe = document.querySelector(`.shoe[color="${color}"]`);
+  let gradient = document.querySelector(`.gradient[color="${color}"]`);
+  let prevGradient = document.querySelector(`.gradient[color="${prevColor}"]`);
 
-  if (isNaN(theNumber) || theNumber < 1 || theNumber > 10) {
-    theMessage = "Number was expected to be betwwen 1 to 10";
+  if (color == prevColor) return;
+
+  colors.forEach((c) => c.classList.remove("active"));
+  this.classList.add("active");
+
+  document.documentElement.style.setProperty("--primary", primary);
+
+  shoes.forEach((s) => s.classList.remove("show"));
+  shoe.classList.add("show");
+
+  gradients.forEach((g) => g.classList.remove("first", "second"));
+  gradient.classList.add("first");
+  prevGradient.classList.add("second");
+
+  prevColor = color;
+  animationEnd = false;
+
+  gradient.addEventListener("animationend", () => {
+    animationEnd = true;
+  });
+}
+
+sizes.forEach((size) => size.addEventListener("click", changeSize));
+colors.forEach((c) => c.addEventListener("click", changeColor));
+
+let x = window.matchMedia("(max-width: 1000px)");
+
+function changeHeight() {
+  if (x.matches) {
+    let shoeHeight = shoes[0].offsetHeight;
+    shoeBg.style.height = `${shoeHeight * 0.9}px`;
   } else {
-    theMessage = "Number is good";
+    shoeBg.style.height = "475px";
   }
-
-  document.getElementById("numberMessage").innerHTML = theMessage;
 }
+
+changeHeight();
+
+window.addEventListener("resize", changeHeight);
